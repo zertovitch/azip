e5d9cec27df3dcd3f9a622858e3f8d7e5fd954f9
gdemont <gdemont@045768f9-7df7-4412-bf8f-cc49bd7eef6e>
2012-11-22 16:06:29 +0000

    User persistence done up to column widths

--- trunk/gui_common/azip_common-user_options.adb
+++ trunk/gui_common/azip_common-user_options.adb
@@ -3,31 +3,47 @@ package body AZip_Common.User_options is
   package body Persistence is
 
     type Key is
-      (
-        view_mode,
+      ( view_mode,
         col_width,
-        maximized,
-        children_maximized
+        win_left, win_top, win_width, win_height,
+        maximized, children_maximized,
+        mru1, mru2, mru3, mru4, mru5, mru6, mru7, mru8, mru9
       );
 
+    pragma Unreferenced (mru2, mru3, mru4, mru5, mru6, mru7, mru8);
 
     procedure Load(opt: out Option_Pack_Type) is
     begin
       for k in Key loop
         begin
           declare
-            ks: constant String:= key'Image(k);
-            s : constant String:= Read_Key(ks);
+            ks: constant Wide_String:= key'Wide_Image(k);
+            s : constant Wide_String:= Read_Key(ks);
           begin
             case k is
               when view_mode =>
-                opt.view_mode:= View_Mode_Type'Value(s);
+                opt.view_mode:= View_Mode_Type'Wide_Value(s);
               when col_width =>
-                null; -- !!
+                for e in Entry_topic loop
+                  opt.column_width(e):= Integer'Wide_Value(
+                    Read_key(ks & "_" & Entry_topic'Wide_Image(e))
+                  );
+                end loop;
+              when win_left =>
+                opt.win_left:= Integer'Wide_Value(s);
+              when win_top =>
+                opt.win_top:= Integer'Wide_Value(s);
+              when win_width =>
+                opt.win_width:= Integer'Wide_Value(s);
+              when win_height =>
+                opt.win_height:= Integer'Wide_Value(s);
               when maximized =>
-                opt.MDI_main_maximized:= Boolean'Value(s);
+                opt.MDI_main_maximized:= Boolean'Wide_Value(s);
               when children_maximized =>
-                opt.MDI_childen_maximized:= Boolean'Value(s);
+                opt.MDI_childen_maximized:= Boolean'Wide_Value(s);
+              when mru1..mru9 =>
+                opt.mru( Key'Pos(k)-Key'Pos(mru1)+1 ):=
+                  To_Unbounded_Wide_String(s);
             end case;
           end;
         exception
@@ -44,21 +60,37 @@ package body AZip_Common.User_options is
     begin
       for k in Key loop
         declare
-          ks: constant String:= key'Image(k);
-          procedure R( v: String ) is
+          ks: constant Wide_String:= key'Wide_Image(k);
+          procedure R( v: Wide_String ) is
           begin
             Write_key(ks, v);
           end R;
         begin
           case k is
             when view_mode =>
-              R(View_Mode_Type'Image(opt.view_mode));
+              R(View_Mode_Type'Wide_Image(opt.view_mode));
             when col_width =>
-              null; -- !!
+              R(""); -- dummy for having reading going well
+              for e in Entry_topic loop
+                Write_key(
+                  ks & "_" & Entry_topic'Wide_Image(e),
+                  Integer'Wide_Image(opt.column_width(e))
+                );
+              end loop;
+            when win_left =>
+              R(Integer'Wide_Image(opt.win_left));
+            when win_top =>
+              R(Integer'Wide_Image(opt.win_top));
+            when win_width =>
+              R(Integer'Wide_Image(opt.win_width));
+            when win_height =>
+              R(Integer'Wide_Image(opt.win_height));
             when maximized =>
-              R(Boolean'Image(opt.MDI_main_maximized));
+              R(Boolean'Wide_Image(opt.MDI_main_maximized));
             when children_maximized =>
-              R(Boolean'Image(opt.MDI_childen_maximized));
+              R(Boolean'Wide_Image(opt.MDI_childen_maximized));
+            when mru1..mru9 =>
+              R( To_Wide_String(opt.mru( Key'Pos(k)-Key'Pos(mru1)+1 )) );
           end case;
         end;
       end loop;
--- trunk/gui_common/azip_common-user_options.ads
+++ trunk/gui_common/azip_common-user_options.ads
@@ -1,16 +1,28 @@
+with Ada.Strings.Wide_Unbounded;        use Ada.Strings.Wide_Unbounded;
+
 package AZip_Common.User_options is
 
   type Column_width_array is array(Entry_topic) of Natural;
 
-  -- The GUI-agnostic part of user options are stored in this record
+  use_default: constant:= -1;
 
+  -- MRU (Most Recently Used) files names:
+  type MRU_List is array(1..9) of Unbounded_Wide_String;
+
+  -- The GUI-agnostic part of user options are stored in this record
+  --
   type Option_Pack_Type is record
     view_mode    : View_Mode_Type:= Flat;
     column_width : Column_width_array:=
       -- Defaults for GWindows. May be scaled for different GUI metrics.
       (Name => 150, Modified => 120, others => 70);
-    MDI_childen_maximized: Boolean:= True;
-    MDI_main_maximized   : Boolean:= True;
+    win_left,
+    win_top,
+    win_width,
+    win_height            : Integer:= use_default;
+    MDI_childen_maximized : Boolean:= True;
+    MDI_main_maximized    : Boolean:= False;
+    mru                   : MRU_List:= (others => Null_Unbounded_Wide_String);
   end record;
 
   -----------------
@@ -21,8 +33,8 @@ package AZip_Common.User_options is
   -- On Linux or Gtk (any platform) it is done usually in a config file
 
   generic
-    with function Read_key(topic: String) return String;
-    with procedure Write_key(topic: String; value: String);
+    with function Read_key(topic: Wide_String) return Wide_String;
+    with procedure Write_key(topic: Wide_String; value: Wide_String);
   package Persistence is
     procedure Load(opt: out Option_Pack_Type);
     procedure Save(opt: in  Option_Pack_Type);

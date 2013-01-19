bb02586ad12ec25cfb68f7f422b014ef91657b60
gdemont <gdemont@045768f9-7df7-4412-bf8f-cc49bd7eef6e>
2013-01-19 21:27:38 +0000

    removed some duplicate #define's numbers in resource id list

--- trunk/gwindows/build.cmd
+++ trunk/gwindows/build.cmd
@@ -5,3 +5,5 @@ gnatmake -P azip_gwindows -XBuild_Mode=Fast
 ren azip.exe AZip.exe
 copy AZip.exe "AZip (ver) win32.exe"
 upx --ultra-brute "AZip (ver) win32.exe"
+copy "AZip (ver) win32.exe" AZip.exe
+

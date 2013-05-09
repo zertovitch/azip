f2d4acad6ced0258445e7dd2b87b0db39b828870
gdemont <gdemont@045768f9-7df7-4412-bf8f-cc49bd7eef6e>
2013-05-09 04:36:44 +0000

    Some details around tree view

--- trunk/gwindows/azip_gwin-mdi_main.adb
+++ trunk/gwindows/azip_gwin-mdi_main.adb
@@ -460,7 +460,7 @@ package body AZip_GWin.MDI_Main is
   begin
     box.Create_Full_Dialog(Window);
     box.Version_label.Text(S2G(Version_info.FileVersion));
-    Create_and_Swap(url_azip, box.AZip_URL, box, "http://sf.net/projects/azip");
+    Create_and_Swap(url_azip, box.AZip_URL, box, "http://azip.sf.net/");
     Create_and_Swap(url_gnat, box.GNAT_URL, box, "http://libre.adacore.com");
     Text(box.GNAT_Version, S2G("version " & CVer.Version));
     Create_and_Swap(url_gnavi, box.GNAVI_URL, box, "http://sf.net/projects/gnavi");

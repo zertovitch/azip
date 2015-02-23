6452768b144923bf465d51b17a9dbd767b2d5fd2
gdemont <gdemont@045768f9-7df7-4412-bf8f-cc49bd7eef6e>
2015-02-23 10:04:11 +0000

    (Updated build script)

--- gwindows/build.cmd
+++ gwindows/build.cmd
@@ -4,10 +4,10 @@ rem e.g. "-march=i686" for MinGW 4.7.2
 REM ADA_INCLUDE_PATH=
 REM C:\Ada\zip-ada\zip_lib;C:\Ada\gnavi\gwindows\framework;C:\Ada\gnavi\gwindows\contrib;C:\Ada\gnavi\gnatcom\framework
 
-gnatmake -P azip_gwindows %1 -XBuild_Mode=Debug
+gprbuild -P azip_gwindows %1 -XBuild_Mode=Debug
 copy azip.exe AZip_Debug.exe
 del azip.exe
-gnatmake -P azip_gwindows %1 -XBuild_Mode=Fast
+gprbuild -P azip_gwindows %1 -XBuild_Mode=Fast
 ren azip.exe AZip.exe
 copy AZip.exe "AZip (ver) win32.exe"
 upx --ultra-brute "AZip (ver) win32.exe"

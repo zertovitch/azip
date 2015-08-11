7152f1a4f85f2272b6cb3a595d66eded3fb0fdfc
gdemont <gdemont@045768f9-7df7-4412-bf8f-cc49bd7eef6e>
2015-08-11 22:22:26 +0000

    (GWindows) Solved MinGW 4.7.2 linker glitch with specific pragma Eliminate

--- trunk/gwindows/build.cmd
+++ trunk/gwindows/build.cmd
@@ -5,13 +5,16 @@ REM ADA_INCLUDE_PATH=
 REM C:\Ada\zip-ada\zip_lib;C:\Ada\gnavi\gwindows\framework;C:\Ada\gnavi\gwindows\contrib;C:\Ada\gnavi\gnatcom\framework
 
 set builder=gnatmake
-set target=GPL
+set target=MinGW
 if (%1)==() set builder=gprbuild
-if (%1)==() set target=MinGW
+if (%1)==() set target=GPL
+if not (%1)==() goto skip_debug
 
 %builder% -P azip_gwindows %1 -XBuild_Mode=Debug
 copy azip.exe AZip_Debug.exe
 del azip.exe
+
+:skip_debug
 %builder% -P azip_gwindows %1 -XBuild_Mode=Fast_%target%
 ren azip.exe AZip.exe
 copy AZip.exe "AZip (ver) win32.exe"
